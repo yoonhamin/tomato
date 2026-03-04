7b590584ae6c610cfa167a067e259e8f8c53faf1
shavrinas <ynhamin@gmail.com>
2026-03-04 15:57:18 +0900

    basic

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def healthcheck():
+    return {"status": "ok", "message": "Flask server is running"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
